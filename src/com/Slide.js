c8bd4581692b3c6af571d0a4f6a1560c57c1efd4
maher-moussi <mahermoussi777@gmail.com>
2021-03-18 10:08:39 +0100

    first commit

---
+++ src/com/Slide.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Slide() {
+    return (
+        <div>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://i.ytimg.com/vi/ZuRVzLmd8Og/maxresdefault.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://tse4.mm.bing.net/th?id=OIP.CnO244Efp8kB7AuGqhxJFwHaEK&pid=Api&P=0&w=296&h=167" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://bd.gaadicdn.com/processedimages/yamaha/yamaha-yzf-r1/source/m_yzf-r1_11548224217.jpg?tr=w-439" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+</div>
+        </div>
+    )
+}
+
+export default Slide
